db10c9adf9985a3937aa0f0b2acca1d4394ff623
jamesalbert <jbert@salsa.alphalyrae.net>
2012-11-14 20:04:34 -0800

    added bar graph and extra grade specs

--- Homework/Help.pm
+++ Homework/Help.pm
@@ -17,27 +17,30 @@ sub get_assignments {
         'dbi:SQLite:dbname=schooldb'
     );
     my $records = $dbh->selectall_arrayref(
-        'select name, date, earned, possible
+        'select name, type, date, earned, possible, grade
             from assignments;', { Slice => {} }
     );
     foreach my $attribute ( @{$records} ) {
         push @assignments, $attribute->{name} . '[ITEMBREAK]';
+        push @assignments, $attribute->{type} . '[ITEMBREAK]';
         push @assignments, $attribute->{date} . '[ITEMBREAK]';
         push @assignments, $attribute->{earned} . '[ITEMBREAK]';
-        push @assignments, $attribute->{possible} . '[NEWITEM]';
+        push @assignments, $attribute->{possible} . '[ITEMBREAK]';
+        push @assignments, $attribute->{grade} . '[NEWITEM]';
     }
     $dbh->disconnect;
     return @assignments;
 }
 
 sub submit_assignment {
-    my ( $self, $assignment, $date, $earned, $possible ) = @_;
+    my ( $self, $assignment, $type, $date, $earned, $possible ) = @_;
     my $dbh = DBI->connect(
         'dbi:SQLite:dbname=schooldb'
     );
+    my $individual_grade = $earned / $possible;
     my $sth = $dbh->prepare(
         "insert into assignments values (
-            null, \"$assignment\", \"$date\", \"$earned\", \"$possible\"
+            null, \"$assignment\", \"$type\", \"$date\", \"$earned\", \"$possible\", \"$individual_grade\"
         );"
     );
     $sth->execute;
@@ -69,6 +72,88 @@ sub get_grade {
     }
     my $grade = $overall_points / $iterations;
     return $grade;
+}
+
+sub get_graph {
+    my $self = shift;
+    use GD::Graph::bars3d;
+    my (
+        @amount_of_tests,
+        @amount_of_homeworks,
+        @amount_of_quizes,
+        @amount_of_projects,
+        @amount_of_extras
+    );
+    my (
+        $total_test_points,
+        $total_homework_points,
+        $total_quiz_points,
+        $total_project_points,
+        $total_extra_points
+    ) = 1;
+    my $dbh = DBI->connect(
+        'dbi:SQLite:dbname=schooldb'
+    );
+    my $test_groups = $dbh->selectall_arrayref(
+        "select earned, possible from assignments
+            where type=\"test\";", { Slice => {} }
+    );
+    my $homework_groups = $dbh->selectall_arrayref(
+        "select earned, possible from assignments
+            where type=\"homework\";", { Slice => {} }
+    );
+    my $quiz_groups = $dbh->selectall_arrayref(
+        "select earned, possible from assignments
+            where type=\"quiz\";", { Slice => {} }
+    );
+    my $project_groups = $dbh->selectall_arrayref(
+        "select earned, possible from assignments
+            where type=\"project\";", { Slice => {} }
+    );
+    my $extra_credit_groups = $dbh->selectall_arrayref(
+        "select earned, possible from assignments
+            where type=\"extra credit\";", { Slice => {} }
+    );
+    foreach my $test ( @{$test_groups} ) {
+        push @amount_of_tests, "$test->{earned}...$test->{possible}";
+        $total_test_points += $test->{earned} / $test->{possible};
+    }
+    foreach my $homework ( @{$homework_groups} ) {
+        push @amount_of_homeworks, "$homework->{earned}...$homework->{possible}";
+        $total_homework_points += $homework->{earned} / $homework->{possible};
+    }
+    foreach my $quiz ( @{$quiz_groups} ) {
+        push @amount_of_quizes, "$quiz->{earned}...$quiz->{possible}";
+        $total_quiz_points += $quiz->{earned} / $quiz->{possible};
+    }
+    foreach my $project ( @{$project_groups} ) {
+        push @amount_of_projects, "$project->{earned}...$project->{possible}";
+        $total_project_points += $project->{earned} / $project->{possible};
+    }
+    foreach my $extra ( @{$extra_credit_groups} ) {
+        push @amount_of_extras, "$extra->{earned}...$extra->{possible}";
+        $total_extra_points += $extra->{earned} / $extra->{possible};
+    }
+    my $overall_test_grade = $total_test_points - 1 / scalar @amount_of_tests if $total_test_points ne 0;
+    my $overall_homework_grade = $total_homework_points - 1 / scalar @amount_of_homeworks if $total_homework_points ne 0;
+    my $overall_quiz_grade = $total_quiz_points - 1 / scalar @amount_of_quizes if $total_quiz_points ne 0;
+    my $overall_project_grade = $total_project_points - 1 / scalar @amount_of_projects if $total_project_points ne 0;
+    my $overall_extra_grade = $total_extra_points - 1 / scalar @amount_of_extras if $total_extra_points ne 0;
+    my @data = (
+        ["tests $overall_test_grade", "homework $overall_homework_grade", "quizes $overall_quiz_grade", "projects $overall_project_grade", "extra credit $overall_extra_grade"],
+        [$overall_test_grade, $overall_homework_grade, $overall_quiz_grade, $overall_project_grade, $overall_extra_grade]
+        #[100, 200, 300, 400, 500]
+    );
+    my $graph = GD::Graph::bars3d->new(800, 600);
+    $graph->set(
+        x_label => "Assignment Types",
+        y_label => "Percentile Range",
+        title   => "How Good You're Doing In Different Respects",
+    );
+    my $gd = $graph->plot( \@data );
+    open(IMG, '>public/graph.png') or die $!;
+    binmode IMG;
+    print IMG $gd->png;
 }
 
 1;
--- backend.pl
+++ backend.pl
@@ -30,11 +30,12 @@ get '/submit/assignment' => sub {
     my $self       = shift;
     my $user       = Homework::Help->new;
     my $assignment = $self->param( 'a' );
+    my $type       = $self->param( 't' );
     my $date       = $self->param( 'd' );
     my $earned     = $self->param( 'e' );
     my $possible   = $self->param( 'p' );
     $user->submit_assignment(
-        $assignment, $date, $earned, $possible
+        $assignment, $type, $date, $earned, $possible
     );
     $self->render( text => 'assignment submitted' );
 };
@@ -50,9 +51,28 @@ get '/get/grade' => sub {
     $self->render( text => $total_points );
 };
 
+get '/get/graph' => sub {
+    my $self   = shift;
+    use Mojo::Server::Morbo;
+    my $user   = Homework::Help->new;
+    my $server = Mojo::Server::Morbo->new;
+    my $status = $user->get_graph;
+    $server->run('backend.pl');
+    #$self->render_static('grade_graph/graph.png');
+    $self->render( 'test' );
+};
+
 app->start;
 
 __DATA__
+
+@@ test.html.ep
+
+<body>
+
+<img src="/graph.png" width="400" height="300"></img>
+
+</body>
 
 @@ home.html.ep
 
@@ -67,10 +87,21 @@ __DATA__
 <body>
 <style>
 input {position: absolute;left: 175px}
+select {position: absolute; left: 177px; width: 173px}
+img {position: absolute; left: 750px}
 </style>
 <h4>Grade Checker</h4>
 
+<img src="/graph.png" width="550" height="400"></img>
+
 Assignment: <input id="assignment" type="text"></input></br>
+Type: <select id="type">
+    <option id="test" value="test">Test</option>
+    <option id="quiz" value="quiz">Quiz</option>
+    <option id="homework" value="homework">Homework</option>
+    <option id="project" value="project">Project</option>
+    <option id="extra_credit" value="extra credit">Extra Credit</option>
+</select></br>
 Date: <input id="date" type="text"></input></br>
 Points Earned: <input id="earned" type="text"></input></br>
 Points Possible: <input id="possible" type="text"></input></br>
@@ -79,9 +110,11 @@ Points Possible: <input id="possible" type="text"></input></br>
 <table id="grade_sheet" border="1">
 <tr>
 <th>ASSIGNMENT</th>
+<th>TYPE</th>
 <th>DATE</th>
 <th>POINTS EARNED</th>
 <th>POINTS POSSIBLE</th>
+<th>GRADE</th>
 </tr>
 </table></br>
 
@@ -95,22 +128,44 @@ Points Possible: <input id="possible" type="text"></input></br>
 $(document).ready(function() {
     $.get('http://localhost:3000/get/grades',
     function(assignments) {
+        $.get('http://localhost:3000/get/graph',
+            function(status) {
+        });
         var record = assignments.split('[NEWITEM]');
         var list_length = record.length;
         for (var i = 0;i < list_length - 1;i++) {
             var attribute = record[i].split('[ITEMBREAK]');
+            var indi_grade = attribute[5];
+            if (indi_grade > 1) {
+                indi_grade = "A++";
+            }
+            else if (indi_grade >= .90 && indi_grade <= 1) {
+                indi_grade = "A";
+            }
+            else if (indi_grade >= .80 && indi_grade < .90) {
+                indi_grade = "B";
+            }
+            else if (indi_grade >= .70 && indi_grade < .80) {
+                indi_grade = "C";
+            }
+            else if (indi_grade >= .60 && indi_grade < .70) {
+                indi_grade = "D";
+            }
+            else {
+                indi_grade = "F";
+            }
             $('#grade_sheet').append(
-                '<tr><th>'+attribute[0]+'</th><th>'+attribute[1]+'</th><th>'+attribute[2]+'</th><th>'+attribute[3]+'</th></tr>'
+                '<tr><th>'+attribute[0]+'</th><th>'+attribute[1]+'</th><th>'+attribute[2]+'</th><th>'+attribute[3]+'</th><th>'+attribute[4]+'</th><th>'+indi_grade+'</th></tr>'
             );
         }
     });
     $.get('http://localhost:3000/get/grade',
     function(response) {
         var grade;
-        if (response > 100) {
+        if (response > 1) {
             grade = "A++";
         }
-        else if (response >= .90 && response <= 100) {
+        else if (response >= .90 && response <= 1) {
             grade = "A";
         }
         else if (response >= .80 && response < .90) {
@@ -128,13 +183,19 @@ $(document).ready(function() {
         $('#grade').val(grade+' '+response)
             .attr('disabled', 'disabled');
     });
+    $('#update_graph').click(function() {
+        $.get('http://localhost:3000/get/graph',
+            function(status) {
+        });
+    });
     $('#submit_assignment').click(function() {
         var assignment = $('#assignment').val();
+        var type = $('#type').val();
         var date = $('#date').val();
         var earned = $('#earned').val();
         var possible = $('#possible').val();
-        if ( assignment != '' && date != '' && earned != '' && possible != '' ) {
-            $.get('http://localhost:3000/submit/assignment?a='+assignment+'&d='+date+'&e='+earned+'&p='+possible,
+        if ( assignment != '' && type != '' && date != '' && earned != '' && possible != '' ) {
+            $.get('http://localhost:3000/submit/assignment?a='+assignment+'&t='+type+'&d='+date+'&e='+earned+'&p='+possible,
             function(status) {
                 alert(status);
                 alert("The page will now reload");
